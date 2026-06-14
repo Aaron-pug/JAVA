e6bf52367db9fb29a7f8e9bbbe8d5484c51090f6
Yangzhican <yyrjn11@163.com>
2026-06-14 17:12:31 +0800

    first commit

---
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,17 @@
+package com.example.demo1;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@MapperScan("com.demo1.mapper")  // 关键：扫描 Mapper 接口
+
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
